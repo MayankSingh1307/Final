946ebb97678d0b15e65947f1f86e37dba3776502
MayankSingh1307 <137820116+MayankSingh1307@users.noreply.github.com>
2023-07-02 11:12:17 +0530

    Update translator.py

--- translator.py
+++ translator.py
@@ -1,13 +1,2 @@
-import unittest
-from translator import translate_english_to_french
-
-class TranslatorTest(unittest.TestCase):
-
-    def test_english_to_french_translation(self):
-        english_text = "Hello, how are you?"
-        expected_french_text = "Bonjour, comment ça va ?"
-        translated_text = translate_english_to_french(english_text)
-        self.assertEqual(translated_text, expected_french_text)
-
-if __name__ == '__main__':
-    unittest.main()
+- name: Run coding standards check
+  run: flake8 translator.py
