9c568fa98daaa8d2d53507bd03371c929c0cb7d7
MayankSingh1307 <137820116+MayankSingh1307@users.noreply.github.com>
2023-07-02 10:55:08 +0530

    Update translator.py

--- translator.py
+++ translator.py
@@ -1,6 +1,13 @@
-from deep_translator import MyMemoryTranslator
+import unittest
+from translator import translate_french_to_english
 
-def translate_french_to_english(text):
-    translator = MyMemoryTranslator(source='fr', target='en')
-    translated_text = translator.translate(text)
-    return translated_text
+class TranslatorTest(unittest.TestCase):
+
+    def test_french_to_english_translation(self):
+        french_text = "Bonjour, comment ça va ?"
+        expected_english_text = "Hello, how are you?"
+        translated_text = translate_french_to_english(french_text)
+        self.assertEqual(translated_text, expected_english_text)
+
+if __name__ == '__main__':
+    unittest.main()
