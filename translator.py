1ee2d05b734b675cdc25ece84ee7c1533a6a2fea
MayankSingh1307 <137820116+MayankSingh1307@users.noreply.github.com>
2023-07-02 10:51:10 +0530

    Update translator.py

--- translator.py
+++ translator.py
@@ -1,6 +1,6 @@
 from deep_translator import MyMemoryTranslator
 
-def translate_english_to_french(text):
-    translator = MyMemoryTranslator(source='en', target='fr')
+def translate_french_to_english(text):
+    translator = MyMemoryTranslator(source='fr', target='en')
     translated_text = translator.translate(text)
     return translated_text
